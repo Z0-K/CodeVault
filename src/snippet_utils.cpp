3c41cb04bd6062a3dce1b19ba2f284faecec60e3
Denis Soke <sokedenis@gmail.com>
2025-06-12 12:05:09 +0200

    modify addSnippet in snippet_utils.cpp

--- src/snippet_utils.cpp
+++ src/snippet_utils.cpp
@@ -1,4 +1,5 @@
 #include "snippet_utils.hpp"
+#include <algorithm>
 
 void addSnippet(std::vector<std::string>& snippets){
     std::string snippet;
@@ -11,6 +12,15 @@ void addSnippet(std::vector<std::string>& snippets){
         std::getline(std::cin, line);
         if (line.empty()) break;
         snippet += line + "\n";
+    }
+
+    bool is_white_space_only = std::all_of(snippet.begin(), snippet.end(), [](char c){
+        return std::isspace(static_cast<unsigned char>(c));
+    });
+
+    if(snippet.empty() || is_white_space_only){
+        std::cout << "Empty or whitespace-only snippet. Not added.\n";
+        return;
     }
 
     snippets.push_back(snippet);
